c155d9137e4c55004c6ee752af492e39b75c4e63
dimat <dmytro.course@gmail.com>
2022-12-29 03:06:56 +0100

    Initial commit

---
+++ src/main/java/com/example/webappfinaltask/WebAppFinalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappfinaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppFinalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppFinalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webappfinaltask/WebAppFinalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappfinaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppFinalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
